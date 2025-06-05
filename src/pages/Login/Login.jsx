2a830b6761c7c1ca7644a0752a4985d1ee0cc391
sajeeb5523 <sajeebaljabed1@gmail.com>
2025-06-05 18:28:21 +0600

    login page done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router';
+import SocialLogin from '../Shared/SocialLogin';
 
 const Login = () => {
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+    };
+
     return (
-        <div>
-            login
+        <div className="min-h-screen flex items-center justify-center bg-gray-100  px-4">
+            <div className="w-full max-w-md bg-white p-8 rounded-xl shadow-lg">
+                <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
+                    Login to Your Account
+                </h2>
+
+                <form onSubmit={handleLogin} className="space-y-5">
+                    {/* Email */}
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700 mb-1">Email Address</label>
+                        <input
+                            type='email'
+                            name='email'
+                            className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                            placeholder="Enter your email"
+                        />
+                    </div>
+
+                    {/* Password */}
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700 mb-1">Password</label>
+                        <input
+                            type='password'
+                            name='password'
+                            className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                            placeholder="Enter your password"
+                        />
+                    </div>
+
+                    {/* Submit Button */}
+                    <button
+                        type='submit'
+                        className="w-full py-3 bg-indigo-600 hover:bg-indigo-700 text-white font-semibold rounded-lg shadow-md transition duration-300">
+                        Login
+                    </button>
+                </form>
+
+                <p className="mt-4 text-center text-sm text-gray-500">
+                    Don’t have an account? <Link to="/register" className="text-indigo-600 hover:underline dark:text-indigo-400">
+                        Register
+                    </Link>
+                </p>
+                <SocialLogin></SocialLogin>
+            </div>
         </div>
     );
 };
