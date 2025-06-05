a07ee138bbc10cb68cf710ef4cd106cc0dba5c5e
sajeeb5523 <sajeebaljabed1@gmail.com>
2025-06-05 18:00:53 +0600

    some navbar code, footer, context code add

---
+++ src/pages/Shared/NavBar.jsx
@@ -0,0 +1,77 @@
+import React, { use } from 'react';
+import { Link, NavLink } from 'react-router';
+import { AuthContext } from '../../context/AuthContext';
+import { TbLogout } from "react-icons/tb";
+
+const NavBar = () => {
+
+    const { user } = use(AuthContext);
+
+    const links = <>
+        <NavLink to='/' className='btn btn-sm'>Home</NavLink>
+        <NavLink className='btn btn-sm'>Fridge</NavLink>
+        {
+            user && <>
+                <NavLink className='btn btn-sm'>Add Food</NavLink>
+                <NavLink className='btn btn-sm'>My Items</NavLink>
+            </>
+        }
+    </>
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-38 shadow space-y-1">
+                        {links}
+                    </ul>
+                </div>
+                <div className='flex items-center gap-2'>
+                    <img src={''} alt="" className='w-15' />
+                    <a className="hidden md:block text-2xl font-bold">FETS bd</a>
+                </div>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 space-x-7 text-base">
+                    {links}
+                </ul>
+            </div>
+            <div className='ml-10'>
+            </div>
+            <div className='login_btn flex items-center gap-2 navbar-end'>
+
+                <div className="relative group">
+                    <img src={`${user ? user.photoURL : ''}`} className='w-12 rounded-full cursor-pointer' alt="" />
+                    {user && (
+                        <div className="absolute right-0 top-full mt-3 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 z-50">
+                            <div className="bg-base-100 shadow-lg rounded-lg w-23 hover:opacity-100 hover:visible">
+                                <ul className="menu menu-sm p-2 gap-1.5">
+                                    <p className='text-center'> {user.displayName}</p>
+                                    <li>
+                                        <button className="btn w-full ">
+                                            <TbLogout />Logout
+                                        </button>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    )}
+                </div>
+
+                {
+                    !user && <>
+                        <NavLink to='/login' className='btn sm:w-auto px-4 py-3 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white text-md md:text-lg rounded-lg hover:scale-105 transition-all duration-300 ease-in-out'>Login</NavLink>
+
+                        <NavLink to='/register' className='btn sm:w-auto px-4 py-3 bg-gradient-to-r from-emerald-400 via-teal-500 to-cyan-500 text-white text-md md:text-lg rounded-lg hover:scale-105 transition-all duration-300 ease-in-out'>Register</NavLink>
+                    </>
+                }
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
---
+++ src/router/router.jsx
@@ -0,0 +1,30 @@
+import {
+    createBrowserRouter
+} from "react-router";
+import RootLayout from "../layouts/RootLayout";
+import Home from "../pages/Home/Home";
+import Register from "../pages/Register/Register";
+import Login from "../pages/Login/Login";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        Component: RootLayout,
+        children: [
+            {
+                index: true,
+                Component: Home,
+            },
+            {
+                path: '/register',
+                Component: Register,
+            },
+            {
+                path: '/login',
+                Component: Login,
+            },
+        ]
+    },
+]);
+
+export default router;
